0bed273b977ebc806f1c582e37ea645eb1176bb0
JGreenOS <jsciteach@gmail.com>
2021-04-05 17:04:22 -0400

    file structure with pseudocode

---
+++ routes/apiRoutes.js
@@ -0,0 +1,21 @@
+//need api route for get workout
+
+
+
+//api route for post workout
+
+
+//api route to get range of days for workout 
+
+
+
+//api route to post range of workouts
+
+
+
+//api route to find and update a workout (resume in progress workout)
+
+
+
+
+
